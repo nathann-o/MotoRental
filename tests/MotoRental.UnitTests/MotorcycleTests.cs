67f3bf8e6764c67ab6d6d3f309448450134be0b4
Nathann Oliveira <nathann_oliveira@hotmail.com.br>
2025-10-07 01:05:12 -0300

    Unit tests

---
+++ tests/MotoRental.UnitTests/MotorcycleTests.cs
@@ -0,0 +1,25 @@
+﻿public class MotorcycleTests
+{
+    [Fact]
+    public void RegisterMotorcycle_ShouldRaiseMotorcycleRegisteredEvent()
+    {
+        var id = Guid.NewGuid();
+        var plate = Plate.Create("ABC1D234");
+        var moto = Motorcycle.Register(id, 2024, "Honda CG", plate);
+
+        Assert.Single(moto.DomainEvents);
+        Assert.IsType<MotorcycleRegisteredEvent>(moto.DomainEvents.First());
+    }
+
+    [Fact]
+    public void UpdatePlate_ShouldChangePlate()
+    {
+        var plate = Plate.Create("AAA1111");
+        var m = Motorcycle.Register(Guid.NewGuid(), 2023, "Model X", plate);
+
+        var newPlate = Plate.Create("BBB2222");
+        m.UpdatePlate(newPlate);
+
+        Assert.Equal("BBB2222", m.Plate.Value);
+    }
+}
---
+++ tests/MotoRental.UnitTests/RentalTests.cs
@@ -0,0 +1,11 @@
+﻿public class RentalTests
+{
+    [Fact]
+    public void CreateRental_StartDateIsCreationPlusOne()
+    {
+        var now = new DateTime(2025, 10, 01);
+        var rental = Rental.Create(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Plan.Days7, now);
+
+        Assert.Equal(now.Date.AddDays(1), rental.StartDate.Date);
+    }
+}
